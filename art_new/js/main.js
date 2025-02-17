4dac7ab1398206253a7b35b08de0cfcf2af2d32c
Popoella <rinjjang1004@naver.com>
2025-02-17 18:02:10 +0900

    250217

--- art_new/js/main.js
+++ art_new/js/main.js
@@ -1,29 +1,35 @@
 $(document).ready(function(){
-    const visual_swiper = new Swiper('.visual .swiper', { 
-        autoplay: { 
-            delay: 5000,
+
+
+    /************* start :: visual의 popup swiper 연결***********/
+    const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
+
+        autoplay: {  /* 팝업 자동 실행 */
+            delay: 1000,
             disableOnInteraction: true,
         },
-        loop: true
+
+        //effect: "fade", /* fade 효과 */
+
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+
+        navigation: {  /* 이전, 다음 버튼 */
+            nextEl: '.visual  .btn_wrap .next',  /* 다음 버튼의 클래스명 */
+            prevEl: '.visual  .btn_wrap .prev',  
         },
-    );
-    
-    $('.visual .ctrl_wrap button.stop').on('click', function(){
-        visual_swiper.autoplay.stop();
-        $(this).hide()
-        $('.visual .ctrl_wrap button.play').show()
-    })
-    $('.visual .ctrl_wrap button.play').on('click', function(){
-        visual_swiper.autoplay.start();
-        $(this).hide()
-        $('.visual .ctrl_wrap button.stop').show()
-    })
-	$('.visual .ctrl_wrap button.next').on('click', function(){
-		visual_swiper.slideNext();
-		visual_swiper.autoplay.start();
-	})
-	$('.visual .ctrl_wrap button.prev').on('click', function(){
-		visual_swiper.slidePrev();
-		visual_swiper.autoplay.start();
-	})
+
+    });
+    /************* end :: visual의 popup swiper 연결***********/
+
+    $('.visual .btn_wrap button.stop').on('click', function(){
+        visual_swiper.autoplay.stop();  /* 일시정지 기능 */
+        $(this).hide();
+        $('.visual .btn_wrap button.play').show();
+	});
+    $('.visual .btn_wrap button.play').on('click', function(){
+        visual_swiper.autoplay.start();  /* 재생 기능 */
+        $(this).hide();
+        $('.visual .btn_wrap button.stop').show();
+	});
+
 })
