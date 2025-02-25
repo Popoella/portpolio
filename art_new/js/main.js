98d35cf40b37c8a32b5a73dd789bd8fe0a3a85bf
Popoella <rinjjang1004@naver.com>
2025-02-25 18:02:17 +0900

    250225

--- art_new/js/main.js
+++ art_new/js/main.js
@@ -23,7 +23,7 @@ $(document).ready(function(){
 
 
 
-     const exhibition_swiper = new Swiper('.exhibition .swiper', { /* 팝업을 감싼는 요소의 class명 */
+    const exhibition_swiper = new Swiper('.exhibition .swiper', { /* 팝업을 감싼는 요소의 class명 */
 
         // autoplay: {  /* 팝업 자동 실행 */
         //     delay: 5000,
@@ -41,6 +41,32 @@ $(document).ready(function(){
         navigation: {  /* 이전, 다음 버튼 */
             nextEl: '.visual  .btn_wrap .next',  /* 다음 버튼의 클래스명 */
             prevEl: '.visual  .btn_wrap .prev',  
+        },
+        breakpoints: {
+            768: {
+                spaceBetween: 30,
+            },
+            0: {
+                spaceBetween: 20,
+            },
+        }
+
+    });
+
+    const event_swiper = new Swiper('.event .swiper', { /* 팝업을 감싼는 요소의 class명 */
+
+    // autoplay: {  /* 팝업 자동 실행 */
+    //     delay: 5000,
+    //     disableOnInteraction: true,
+    // },
+
+    //effect: "fade", /* fade 효과 */
+
+        loop: false,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        
+        navigation: {  /* 이전, 다음 버튼 */
+        nextEl: '.event .culture .tit .btn_wrap .next',  /* 다음 버튼의 클래스명 */
+        prevEl: '.event .culture .tit .btn_wrap .prev',  
         },
 
     });
@@ -67,5 +93,15 @@ $(document).ready(function(){
     $('.exhibition .list .swiper ul li').on('mouseleave', function(){
         $(this).removeClass('hover')
     })
+
+
+
+    $('.event .culture').on('mouseenter focusin', function(){
+        $(this).addClass('hover')
+    })
+    $('.event .culture').on('mouseleave', function(){
+        $(this).removeClass('hover')
+    })
+
 
 })
